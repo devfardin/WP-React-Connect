af8177a623a97e439d7d8076ffa946b60a9e14a5
devfardin <contactfardin22@gmail.com>
2025-11-13 17:52:20 +0600

    create a wp react connector plugin

---
+++ wp-react-connect.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: ReactPress Bridge
+ * Plugin URI: https://fardin-ahmed.netlify.app/
+ * Description: A lightweight WordPress plugin to connect React frontends with WordPress and WooCommerce through REST API, providing secure endpoints and better integration support.
+ * Version: 1.0.0
+ * Author: Fardin Ahmed
+ * Author URI: https://github.com/devfardin
+ * License: GPL2
+ */
